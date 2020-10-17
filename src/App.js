8c31735cd5b7c112d80cd558013e1ae6480798dd
JoseP-UR <urivesrosa@gmail.com>
2020-10-17 19:55:55 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { hot } from "react-hot-loader";
 import "./App.sass";
+import img from './assets/test.png'
+import Navbar from './components/Navbar'
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <header className="header">this is the header</header>
-        <div className="sidebar"> this is the sidebar</div>
-        <div className="main"> this is the main area</div>
+        <Router>
+          <Navbar />
+
+          <Switch>
+            <Route path="/">
+              <div className="main"> this is the main area
+          <img className="imagem" src={img}></img>
+                <div>home</div>
+                <div>home</div>
+                <div>home</div>
+                <div>home</div>
+              </div>
+            </Route>
+            <Route path="/about">
+              <div className="main"> this is the about area
+                <div>about</div>
+                <div>about</div>
+                <div>about</div>
+                <div>about</div>
+              </div>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,8 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
           'style-loader',
-          // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
           {
             loader: 'sass-loader',
             options: {
@@ -31,7 +28,15 @@ module.exports = {
             }
           },
         ],
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
